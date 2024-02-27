9f6da38d800c53e5864e7fea171827c60c7d91fd
cfculler <carsonculler2020@gmail.com>
2024-02-26 22:06:14 -0800

    feat: song lyrics now fetch and populate in a text file

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,110 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+const axios = require('axios');
+const genius = require('genius-lyrics-api');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const geniusApiKey = '3NVoN8M9F83zCC3Yr43380lZ-uepnzvjMD1koGMN0f7CT66YUlV8HF_7ZA5fYZ-a';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tab-scroller" is now active!');
+async function fetchLyrics(songName: string, artistName: string) {
+  const options = {
+    apiKey: geniusApiKey,
+    title: songName,
+    artist: artistName,
+    optimizeQuery: true
+  };
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tab-scroller.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Tab Scroller!');
-	});
+  let lyrics = "";
+  
+  try {
+    await genius.getLyrics(options).then((text: string) => (lyrics = text));
+  } catch(error) {
+    console.error('Error retrieving song lyrics');
+    return;
+  }
 
-	context.subscriptions.push(disposable);
+  return lyrics;
+}
+
+async function createAndOpenFile(fileName: string, lyrics: string) {
+
+  if (vscode.workspace.workspaceFolders !== undefined) {
+    const filePath = vscode.workspace.workspaceFolders[0].uri.path + '/tabs/' + fileName + '.txt';
+    try {
+      fs.writeFileSync(filePath, lyrics);
+      console.log('File created successfully.');
+      vscode.window.showInformationMessage('New file created at ' + filePath);
+    } catch (err) {
+        console.error('Error creating file:', err);
+    }
+
+    try {
+        const document = await vscode.workspace.openTextDocument(filePath);
+        vscode.window.showTextDocument(document);
+    } catch (err) {
+        console.error('Error opening file: ', err);
+    }
+  } else {
+      vscode.window.showWarningMessage('Error: Please open a workspace before running command');
+  }
+}
+
+function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "tab-scroller" is now active!');
+
+    let disposable = vscode.commands.registerCommand('tab-scroller.helloWorld', async () => {
+
+      const userInput = await vscode.window.showInputBox({
+        prompt: 'Type the name of a song',
+        placeHolder: 'Name that tune...',
+        value: '', 
+      });
+
+      const userInput2 = await vscode.window.showInputBox({
+        prompt: 'Who\'s the song by?',
+        placeHolder: 'Name that artist...',
+        value: '', 
+      });
+
+      let artistName = "";
+
+      if (userInput2 !== undefined) {
+        artistName = userInput2;
+      }
+
+      if (userInput !== undefined) {
+        const songName = userInput;
+        let lyrics = "";
+        try {
+          const text = await fetchLyrics(songName, artistName);
+          if (text !== undefined) {
+            lyrics = text;
+          }
+        } catch(error) {
+          vscode.window.showWarningMessage('Looks like we can\'t find lyrics to that song. Try again!');
+          console.error('Error fetching lyrics');
+        }
+        const fileName = songName.toLowerCase().replace(/ /g, '-');
+        if (lyrics !== "") {
+          try {
+            await createAndOpenFile(fileName, lyrics);
+          } catch(error) {
+            console.error('Error creating file');
+          }
+        }
+
+      } else {
+        vscode.window.showWarningMessage('User canceled the input.');
+      }
+
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
